9d4f91bf48e4e29b4e49293556bec161af5925c0
MohamedELbohy1 <elbohym33@gmail.com>
2025-10-12 16:09:04 +0200

    In Progress 🚗

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,4 +1,4 @@
-import SideBar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map";
 import User from "../components/User";
@@ -6,7 +6,7 @@ import User from "../components/User";
 function AppLayout() {
   return (
     <div className={styles.app}>
-      <SideBar />
+      <Sidebar />
       <Map />
       <User />
     </div>
