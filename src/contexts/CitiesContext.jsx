0bc0bb936936d0f71d90839c884e8c0cc797cafe
MohamedELbohy1 <elbohym33@gmail.com>
2025-10-18 00:57:02 +0200

    Modify

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,7 +7,8 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+// const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://world-wise-onlive.vercel.app/";
 
 const CitiesContext = createContext();
 
